6f8bbb257f5a0262a6baa2967bc237b11923efa9
zxldev <zxldev@live.com>
2017-01-23 23:25:17 +0800

    hello

---
+++ src/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("hello Go")
+	fmt.Println("rand number:", rand.Intn(100))
+	fmt.Println("Pi is value:", math.Pi)
+}
